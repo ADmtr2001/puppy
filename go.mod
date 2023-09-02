ce9f4b306d94fccfaa2c82e3f9e4168e2ea31bb5
Arsen Dmytrenko <arsen.dmitrenko1994@gmail.com>
2023-09-02 22:08:12 +0300

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ADmtr2001/puppy
 
 go 1.20
+
+require github.com/ADmtr2001/dog v0.0.0-20230902190143-a804a29c9254 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/ADmtr2001/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrowUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrowUp(Barks())
 }
